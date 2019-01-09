01735129f25602c3d1a8b533358272d9948a069e
James Roman <platinio94@gmail.com>
2019-01-09 09:49:15 -0300

    small changes

--- Assets/PlatinioUITween/Scripts/UIMoveAnimation.cs
+++ Assets/PlatinioUITween/Scripts/UIMoveAnimation.cs
@@ -10,49 +10,43 @@ namespace PlatinioUITweeen
     [System.Serializable]
     public class UIMoveAnimation : Animation
     {       
-        [SerializeField] private List<Vector2> m_path = null;
-        [SerializeField] private List<float> m_pathTime = null;
+        [SerializeField] private List<Vector2>      m_path      = null;
+        [SerializeField] private List<float>        m_pathTime  = null;
 
         private List<Vector2>   m_processedPath         = null;        
         private int             m_currentPathIndex      = 0;
         private List<float>     m_pathDistance          = new List<float>();
-        private float           m_acumulatePathDistance = 0.0f;
+        private float           m_totalPathDistance      = 0.0f;
         private Vector2         m_startPosition         = Vector2.zero;
 
-        public List<Vector2> ProcessedPath
-        {
-            get
-            {
-                return ProcessPath(m_path);
-            }
-        }
-
-        public List<Vector2> Path { get { return m_path; } set { m_path = value; } }
+        public List<Vector2> ProcessedPath  { get { return ProcessPath(m_path); }                           }
+        public List<Vector2> Path           { get { return m_path;              } set { m_path = value; }   }
 
        
         public override void Construct(UIAnimator animator)
         {
             base.Construct(animator);
 
+            //process the path
             m_processedPath = ProcessPath(m_path);
 
             //first get path distance
-            m_pathDistance = new List<float>();
-            m_acumulatePathDistance = 0.0f;
+            m_pathDistance      = new List<float>();
+            m_totalPathDistance = 0.0f;
 
             for (int n = 0; n < m_processedPath.Count - 1; n++)
             {
                 float d = Vector2.Distance(m_processedPath[n], m_processedPath[n + 1]);
 
                 m_pathDistance.Add( d );
-                m_acumulatePathDistance += d;
+                m_totalPathDistance += d;
             }
 
             m_pathTime = new List<float>();
 
             for (int n = 0 ; n < m_processedPath.Count - 1 ; n++)
             {
-                m_pathTime.Add( (m_pathDistance[n] / m_acumulatePathDistance) * m_length );
+                m_pathTime.Add( (m_pathDistance[n] / m_totalPathDistance) * m_length );
                 Debug.Log(m_pathTime[n]);
             }
         }
@@ -60,12 +54,11 @@ namespace PlatinioUITweeen
         public override void Play(bool loop , float t)
         {
             m_startPosition = m_rect.anchoredPosition;
-
             m_length = t;
 
             base.Play(loop , t);
-            m_currentPathIndex = 0;
 
+            m_currentPathIndex = 0;
             GoToNextNode();
         }
 
@@ -73,7 +66,7 @@ namespace PlatinioUITweeen
         {
             float distanceToNextNode = Vector2.Distance(m_processedPath[m_currentPathIndex], m_processedPath[m_currentPathIndex + 1]);
 
-            PlatinioTween.instance.Vector3Tween(m_processedPath[m_currentPathIndex], m_processedPath[m_currentPathIndex+1] , (distanceToNextNode / m_acumulatePathDistance) * m_length ).SetOnUpdate(delegate(Vector3 v) 
+            PlatinioTween.instance.Vector3Tween(m_processedPath[m_currentPathIndex], m_processedPath[m_currentPathIndex+1] , (distanceToNextNode / m_totalPathDistance) * m_length ).SetOnUpdate(delegate(Vector3 v) 
             {
                 m_rect.anchoredPosition = v;
             }).SetOnComplete(delegate 
@@ -95,7 +88,7 @@ namespace PlatinioUITweeen
                                         
                     #else
                     if(m_loop)
-                        Play(m_loop);
+                        Play(m_loop , m_length);
                     #endif
 
                 }
@@ -122,7 +115,7 @@ namespace PlatinioUITweeen
             t -= passTime;
 
             Vector3 change = m_processedPath[index + 1] - m_processedPath[index];
-            m_rect.anchoredPosition = Equations.ChangeVector(t, m_processedPath[index], change , (distanceToNextNode / m_acumulatePathDistance) * m_length , m_ease);
+            m_rect.anchoredPosition = Equations.ChangeVector(t, m_processedPath[index], change , (distanceToNextNode / m_totalPathDistance) * m_length , m_ease);
         }
 
         private int GetIndexAtTime(float t)
@@ -149,8 +142,6 @@ namespace PlatinioUITweeen
                 processedPath.Add(Vector2.Scale(m_path[n] - m_rect.anchorMin, m_animator.Canvas.sizeDelta));
 
             }
-
-
 
             return processedPath;
         }
